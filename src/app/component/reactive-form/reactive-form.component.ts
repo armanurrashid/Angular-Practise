6b3533bef5d86a4cf8072656456a85b1ae42096b
Armanur Rashid <armanurrashid105086@gmail.com>
2025-05-19 00:18:33 +0600

    Reactive Form

--- src/app/component/reactive-form/reactive-form.component.ts
+++ src/app/component/reactive-form/reactive-form.component.ts
@@ -1,11 +1,27 @@
+import { CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule , FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [],
+  imports: [ReactiveFormsModule,JsonPipe, CommonModule ],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent {
+studentForm: FormGroup = new FormGroup({
+firstName: new FormControl('',[Validators.required,Validators.minLength(4)]),
+lastName: new FormControl('',[Validators.required,Validators.minLength(4)]),
+userName: new FormControl('',),
+city: new FormControl(''),
+state: new FormControl(''),
+zipCode: new FormControl(''),
+isAcceptTerms: new FormControl(false),
+})
 
+formValue:any;
+
+onSave(){
+  this.formValue = this.studentForm.value;
+}
 }
